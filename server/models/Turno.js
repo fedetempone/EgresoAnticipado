c475f902389ed94caf89ce528e9158a050ecadda
fedetempone <fedetempone5@gmail.com>
2025-03-12 16:17:26 -0300

    new user registration error fixed, trying to fix new error in shift access

--- server/models/Turno.js
+++ server/models/Turno.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const turnoSchema = new mongoose.Schema({
   Lunes: { type: [String], default: Array(8).fill('') },
   Martes: { type: [String], default: Array(8).fill('') },
-  Miércoles: { type: [String], default: Array(8).fill('') },
+  Miercoles: { type: [String], default: Array(8).fill('') },
   Jueves: { type: [String], default: Array(8).fill('') },
   Viernes: { type: [String], default: Array(8).fill('') },
 });
